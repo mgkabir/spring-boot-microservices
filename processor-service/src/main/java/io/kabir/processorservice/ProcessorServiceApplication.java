ebb309e589aad4e0a8b6c096353bd2d2f3332ea3
Mohammad Kabir <mgkabir@gmail.com>
2019-08-30 14:27:04 +1000

    processor service running

---
+++ processor-service/src/main/java/io/kabir/processorservice/ProcessorServiceApplication.java
@@ -0,0 +1,15 @@
+package io.kabir.processorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessorServiceApplication.class, args);
+	}
+
+}

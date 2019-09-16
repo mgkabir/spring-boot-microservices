2a186419450e84968cfd953f2298b24df58ebc32
Mohammad Kabir <mgkabir@gmail.com>
2019-09-16 13:13:51 +1000

    using consul in tdb-service

--- tdb-service/src/main/java/io/kabir/tdbservice/TdbServiceApplication.java
+++ tdb-service/src/main/java/io/kabir/tdbservice/TdbServiceApplication.java
@@ -2,10 +2,8 @@ package io.kabir.tdbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class TdbServiceApplication {
 
 	public static void main(String[] args) {

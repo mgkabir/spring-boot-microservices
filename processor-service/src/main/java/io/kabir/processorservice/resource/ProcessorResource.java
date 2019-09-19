79285d68f43057b3469a974b784d2812b140a4e1
Mohammad Kabir <mgkabir@gmail.com>
2019-09-19 12:38:47 +1000

    inspect discoveryClient values

--- processor-service/src/main/java/io/kabir/processorservice/resource/ProcessorResource.java
+++ processor-service/src/main/java/io/kabir/processorservice/resource/ProcessorResource.java
@@ -1,6 +1,8 @@
 package io.kabir.processorservice.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +12,22 @@ public class ProcessorResource {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @GetMapping("/ping")
-    public String getPingFromTDB(){
-        return this.restTemplate.getForObject("http://triplestore-service/ping",String.class);
+    public String getPingFromTDB() {
+        /* Inspect some values using discoveryClient */
+        discoveryClient
+                .getInstances("triplestore-service")
+                .stream()
+                .forEach(this::displayServiceInstanceDetails);
+
+        return this.restTemplate.getForObject("http://triplestore-service/ping", String.class);
+    }
+
+    private void displayServiceInstanceDetails(ServiceInstance serviceInstance) {
+        System.out.println("URI : " + serviceInstance.getUri().toString() + " InstanceId : " + serviceInstance.getInstanceId() + " ServiceId : " + serviceInstance.getServiceId());
+        serviceInstance.getMetadata().forEach((k, v) -> System.out.println("Key : " + k + " Value : " + v));
     }
 }
